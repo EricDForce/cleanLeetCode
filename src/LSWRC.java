83b3560caaea64e94aff9ea6a94ce192ffdf14e0
Eric_D <867357234@qq.com>
2017-04-09 15:25:32 +0800

    again

--- src/LSWRC.java
+++ src/LSWRC.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,22 +12,28 @@ public class LSWRC {
     }
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        int i=0, j=0, n=s.length(),maxlen = Integer.MIN_VALUE;
+        int j=0, n=s.length(),maxlen = Integer.MIN_VALUE;
         if(n==0) return 0;
-        while (i<n && j<n){
-            if (map.get(s.charAt(i))==null){
-                map.put(s.charAt(i), i);
-                maxlen = Math.max(i - j + 1, maxlen);
-                i++;
-            }else{
-                if (map.get(s.charAt(i))>=j)
-                    j=map.get(s.charAt(i))+1;
-                else
-                    maxlen = Math.max(i - j + 1, maxlen);
-                map.put(s.charAt(i), i);
-                i++;
-            }
+
+        for (int i=0;i<n;i++){
+            if (map.get(s.charAt(i)) != null && map.get(s.charAt(i))>=j)
+                j=map.get(s.charAt(i))+1;
+            map.put(s.charAt(i), i);
+            maxlen = Math.max(i - j + 1, maxlen);
         }
         return maxlen;
+    }
+    public int lengthOfLongestSubstring2(String s) {
+        int[] charMap = new int[256];       //对于字符类型的hashMap使用整形数组即可，字符就是8位整形
+        Arrays.fill(charMap, -1);
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (charMap[s.charAt(j)] >= i) {
+                i = charMap[s.charAt(j)] + 1;
+            }
+            charMap[s.charAt(j)] = j;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
     }
 }
