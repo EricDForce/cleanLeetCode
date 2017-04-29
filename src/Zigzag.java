81b929577bdc471b1e0625d464d8c93753c869f7
Eric_D <867357234@qq.com>
2017-04-29 19:59:08 +0800

    LeetCode
    LeetCode

--- src/Zigzag.java
+++ src/Zigzag.java
@@ -11,43 +11,27 @@ public class Zigzag {
     }
     public String convert(String s, int numRows) {
         Map<Integer, String> map = new HashMap<>();
-        boolean bool  = true;  //向下
+        boolean bool  = false;  //向下
         int index = 0;
         int t = 0;
         while(index < s.length())
         {
-            if((t+1) == numRows){
-                bool = false;
-            }else if(t==0){
-                bool = true;
+            if((t+1) == numRows || t==0){
+                bool = !bool;
             }
-            if(bool){
-                if(map.get(t) == null){
-//                    System.out.println("----1d"+s.charAt(index));
-                    map.put(t, ""+s.charAt(index));
-                }else{
-//                    System.out.println("----2d"+s.charAt(index));
-                    map.put(t, map.get(t)+s.charAt(index));
-                }
+            map.put(t, map.get(t)==null ? ""+s.charAt(index) : map.get(t)+s.charAt(index));
+
+            if (bool){
                 t++;
-            }else{
-                if(map.get(t) == null){
-//                    System.out.println("----3d"+s.charAt(index));
-                    map.put(t, ""+s.charAt(index));
-                }else{
-//                    System.out.println("----4d"+s.charAt(index));
-                    map.put(t, map.get(t)+s.charAt(index));
-                }
+            }else {
                 t--;
             }
             index++;
         }
         StringBuilder res = new StringBuilder();
         for(int i : map.keySet()){
-            System.out.println("------"+i+":"+map.get(i));
             res.append(map.get(i));
         }
-
         return res.toString();
     }
 }
--- src/atoi.java
+++ src/atoi.java
@@ -2,6 +2,11 @@
  * Created by eric-d on 2017/4/8.
  */
 public class atoi {
+    public static void main(String[] args){
+        System.out.println("max : " + Integer.MAX_VALUE);
+        System.out.println("min : " + Integer.MIN_VALUE);
+        System.out.println(new atoi().myAtoi("2147483648"));
+    }
     public int myAtoi(String str) {
         int i=0, n=str.length();
         int numDiv10 = Integer.MAX_VALUE/10;
@@ -16,7 +21,7 @@ public class atoi {
         int num = 0;
         while (i<n && Character.isDigit(str.charAt(i))){
             int digit = Character.getNumericValue(str.charAt(i));
-            if (num >numDiv10 || num==numDiv10 && digit>8){      //  &&优先级高于||
+            if (num >numDiv10 || num==numDiv10 && digit>=8){      //  &&优先级高于||
                 return sign==1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             num = num*10+digit;
