9e0dc2814245047799c6e2a3b45398167e6999d8
xxx <xxx@xxx.com>
2017-06-15 18:00:57 +0800

    just for fun

---
+++ src/JustForTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by eric-d on 2017/6/15.
+ */
+public class JustForTest {
+    public static void main(String[] args){
+        String str = "dengli";
+        System.out.println("output: " + str);
+    }
+}
