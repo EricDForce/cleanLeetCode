1eade5507d84503a22868f569115ae5e37b3ba57
xxx <xxx@xxx.com>
2017-06-20 21:59:11 +0800

    just for fun

---
+++ src/MinimumWindowSubstring.java
@@ -0,0 +1,42 @@
+/**
+ * Created by eric-d on 2017/6/20.
+ */
+public class MinimumWindowSubstring {
+    public static void main(String[] args){
+        String s = "A";
+        String t = "B";
+        System.out.println("string:"+new MinimumWindowSubstring().minWindow(s, t));
+    }
+
+    public String minWindow(String s, String t) {
+        char[] st = t.toCharArray();
+        int[] charMap = new int[256];
+        int[] curMap = new int[256];
+        for (int i=0;i<t.length();i++){
+            charMap[t.charAt(i)]++;
+        }
+        int start = 0, cnt = t.length(), MX = Integer.MAX_VALUE;
+        String result = "";
+        while (start < s.length()&&charMap[s.charAt(start)]==0) start++;
+        for (int i=start;i<s.length();i++){
+            if (charMap[s.charAt(i)] != 0){
+                curMap[s.charAt(i)]++;
+                if (curMap[s.charAt(i)] <= charMap[s.charAt(i)])
+                cnt--;
+
+                if (cnt==0){
+                    while (start < s.length()&&(charMap[s.charAt(start)]==0 || curMap[s.charAt(start)]>charMap[s.charAt(start)])) {
+                        if (curMap[s.charAt(start)]>charMap[s.charAt(start)])
+                        curMap[s.charAt(start)]--;
+                        start++;
+                    }
+                    if ((i-start) < MX){
+                        MX = (i-start);
+                        result = s.substring(start, i+1);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
