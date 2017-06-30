6f085abf94a0169451f8d0773bdab9128b13a82c
xxx <xxx@xxx.com>
2017-06-30 08:57:15 +0800

    just for fun

---
+++ src/DistinctSubsequences.java
@@ -0,0 +1,13 @@
+/**
+ * Created by eric-d on 2017/6/29.
+ */
+public class DistinctSubsequences {
+    public static void main(String[] args)
+    {
+
+    }
+    public int numDistinct(String s, String t) {
+        
+        return 0;
+    }
+}
