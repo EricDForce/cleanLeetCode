9241925e81a9a5b861dba4c34b330a40b703b493
xxx <xxx@xxx.com>
2017-06-30 19:53:25 +0800

    just for fun

--- src/DistinctSubsequences.java
+++ src/DistinctSubsequences.java
@@ -7,7 +7,13 @@ public class DistinctSubsequences {
 
     }
     public int numDistinct(String s, String t) {
-        
+        int l1 = s.length(), l2 = t.length();
+        int[][] dp = new int[l1+1][l2+1];
+        for (int i=0;i<=l1;i++){
+            for (int j=0;j<=l2;j++){
+                
+            }
+        }
         return 0;
     }
 }
