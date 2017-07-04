7614a91b91d7d3ff44e08211adf1d840c57f47bc
xxx <xxx@xxx.com>
2017-07-04 15:39:18 +0800

    just for fun

--- src/grayCode.java
+++ src/grayCode.java
@@ -3,6 +3,7 @@
  */
 import java.util.*;
 import java.math.*;
+
 public class grayCode {
     public static void main(String[] args)
     {
@@ -12,22 +13,17 @@ public class grayCode {
         {
             System.out.println(iter);
         }
-
     }
 
     public List<Integer> grayCode(int n) {
         List<Integer> tmp = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
-
-        if(n==1)
-        {
+        if(n==1) {
             tmp.add(0);
             tmp.add(1);
             return tmp;
         }
-
         tmp = grayCode(n-1);
-
         result.addAll(tmp);
         for(int i=tmp.size()-1;i>=0;i--)
         {
