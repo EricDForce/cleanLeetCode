cd3e6d1d9176756f4c4f3274f6772b59080e9e40
xxx <xxx@xxx.com>
2017-08-03 20:28:17 +0800

    just for fun

--- src/dataMedian.java
+++ src/dataMedian.java
@@ -1,10 +1,10 @@
-import java.util.PriorityQueue;
 import java.util.*;
 
 public class dataMedian {
     public static PriorityQueue<Integer> min = new PriorityQueue();
 //    PriorityQueue<Integer> max = new PriorityQueue(1000, Collections.reverseOrder());
-//当JDK8的时候，可以直接写成，不再需要初始化容量
+    //当JDK8的时候，可以直接写成，不再需要初始化容量
+    //数据流取中位数，插入O(lgn),取中位数O(1)
     public static PriorityQueue<Integer> max = new PriorityQueue(Collections.reverseOrder());
     public static void main(String[] args){
         Integer[] nums = {1,2,5,7,9,8,4,5};
