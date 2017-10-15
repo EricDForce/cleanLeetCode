b4b48cbcadfacfdb31dde7285c87166601ff1c77
xxx <xxx@xxx.com>
2017-10-15 16:58:38 +0800

    just for fun

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,8 +2,7 @@
  * Created by eric-d on 2017/6/25.
  */
 public class QuickSort {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] nums = {1,9,7,8,3,5,4,7,10,2,40};
         new QuickSort().quicksort(nums, 0, nums.length-1);
         for (int i : nums)
@@ -12,18 +11,15 @@ public class QuickSort {
         }
 //        System.out.println(new QuickSort().climbStairs(44));
     }
-    public void quicksort(int[] nums, int l, int r)
-    {
+    public void quicksort(int[] nums, int l, int r) {
         if (nums==null || l < 0)
             return;
         int pivot;
-        if (l < r)
-        {
+        if (l < r) {
             pivot = partition(nums, l, r);
             quicksort(nums, l, pivot-1);
             quicksort(nums, pivot+1, r);
         }
-
     }
     public int partition(int[] nums, int l, int r)
     {
--- src/baidu1.java
+++ src/baidu1.java
@@ -1,5 +1,1 @@
-public class baidu1 {
-    public static void main(String[] args){
 
-    }
-}
