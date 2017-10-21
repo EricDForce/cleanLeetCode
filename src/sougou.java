1f55f5addf8f54cf15088a21882ea5c43a42e820
xxx <xxx@xxx.com>
2017-10-21 16:33:18 +0800

    just for fun

--- src/sougou.java
+++ src/sougou.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class sougou {
     public static void main(String[] args){
-        int[][] nums = {{2, 3},{3, 4},{5, 6},{7, 8}};
+        int[][] nums = {{2, 3}, {3, 4}, {5, 6}, {7, 8}};
         List<Set<Integer>> list = new ArrayList<>();
         new sougou().build(nums, list);
         for (int i=0; i<list.size(); i++){
