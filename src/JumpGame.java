103902403ba2550e3b38ed4fc6b0d39db9913959
dengli <dengli1@xiaomi.com>
2019-01-12 21:23:58 +0800

    增加jump game中能到最后最少步数

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -4,8 +4,9 @@
 public class JumpGame {
 
     public static void main(String[] argv) {
-        int[] nums = new int[]{3, 2, 1, 1, 4};
-        System.out.println(new JumpGame().canJump2(nums));
+        //int[] nums = new int[]{2, 3, 0, 1, 4};
+        int[] nums = new int[]{2, 3, 1, 1, 4};
+        System.out.println(new JumpGame().jump2(nums));
     }
     //[2,3,1,1,4] => true; [3,2,1,0,4] => false
     //dynamic programming编程
@@ -34,5 +35,49 @@ public class JumpGame {
         }
         if (leftGood==0) return true;
         return false;
+    }
+
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] jumpNum = new int[n];
+        jumpNum[n-1] = 0;
+        for (int i=n-2; i>=0; i--) {
+            jumpNum[i] = Integer.MAX_VALUE;
+            int min = Integer.MAX_VALUE;
+            if ((i+nums[i])>=(n-1)) {
+                jumpNum[i] = 1;
+            } else {
+                for (int j=i+1; j<=(i+nums[i]); j++) {
+                    min = Math.min(jumpNum[j], min);
+                }
+                if (min!=Integer.MAX_VALUE) {   //避免上面for循环的逻辑不走导致min+1溢出
+                    jumpNum[i] = min + 1;
+                }
+            }
+        }
+        return jumpNum[0];
+    }
+
+    public int jump2(int[] nums) {
+        if (nums.length==1) return 0;
+        int cur = 0, end = 0, farthest = 0, n=nums.length, jump = 0;
+        while (cur <= end) {
+            if (farthest < (nums[cur] + cur)) {
+                farthest = nums[cur] + cur;
+            }
+            if (farthest >= n-1) {
+                return (jump+1);
+            }else if (cur==end){
+                if (farthest > end) {
+                    jump++;
+                    end = farthest;
+                } else {
+                    break;    //farthest<=end说明跳不出去了，不能跳到最后
+                }
+            } else {
+                cur++;
+            }
+        }
+        return 0;
     }
 }
