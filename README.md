eb570580dd31ef3721497fbb260fff03769bfbe1
dengli <dengli1@xiaomi.com>
2019-06-06 10:36:08 +0800

    增加algorithmcode目录

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cleanLeetCode
 some leetcode problems with short and clean solutions
 just for fun!
+
---
+++ src/algorithmcode/BinarySearch.java
@@ -0,0 +1,26 @@
+package algorithmcode;
+
+/**
+ * @author : dengli
+ * @date : 2019/05/17.
+ */
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,6,8,9,45,67,87};
+		System.out.println(BinarySearch.binarySearch(nums, 89));
+	}
+
+	public static int binarySearch(int[] nums, int x) {
+		int left = 0, right = nums.length-1;
+		while (left < right) {
+			int mid = (left + right)/2;
+			if (nums[mid]==x) {
+				return mid;
+			}
+			left = (nums[mid] > x) ? left : mid;
+			right = (nums[mid] > x) ? mid : right;
+		}
+		return -1;
+	}
+}
